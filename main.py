674cf2cab84bee29b20c06997015d1e60ec4869d
osumico <dev01.io.sem@gmail.com>
2022-05-14 20:19:39 +0300

    Small fix

--- main.py
+++ main.py
@@ -4,19 +4,19 @@ import front_cls as UI
 
 
 player = P.Player(100)
+dealer = P.Player(100)
+
 deck = C.Deck()
 deck.shuffle()
 
 for _ in range(2):
     player.to_hand(deck.draw_card())
-    
-player.money(30, False)
-print(player.extract_stat())
 
-
-dealer = P.Player(100)
 for _ in range(2):
     dealer.to_hand(deck.draw_card())
     
+player.money(30, False)
 dealer.money(30, False)
+
+print(player.extract_stat())
 print(dealer.extract_stat())
